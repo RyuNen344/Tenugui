a313c29368e1922e6305d8758fd54c0af0524f4f
RyuNen344 <s1100633@outlook.com>
2025-03-09 13:26:36 +0900

    chore: lint

--- tests/src/androidTest/java/io/github/ryunen344/tenugui/tests/rule/SystemUiRule.kt
+++ tests/src/androidTest/java/io/github/ryunen344/tenugui/tests/rule/SystemUiRule.kt
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
- * Copyright (C) 2025-2025 RyuNen344
+ * Copyright (C) 2025 RyuNen344
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -112,6 +112,7 @@ class SystemUiRule(private val animate: Boolean = false) : TestRule {
         countDownLatch.await(10, TimeUnit.SECONDS)
     }
 
+    @Suppress("UNUSED", "UnusedPrivateMember")
     private fun Context.demoMode(enable: Boolean) {
         Settings.Global.putInt(contentResolver, SYSTEM_UI_DEMO_ALLOWED, if (enable) 1 else 0)
         Settings.Global.putInt(contentResolver, SYSTEM_UI_TUNER_DEMO_ON, if (enable) 1 else 0)
